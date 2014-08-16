4d8fe5cf306f47fff8938c9e5ba357b93a65cf22
Joel Pedraza <github@saik0.net>
2014-08-16 14:38:15 -0400

    0.0.5 release

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='humblebundle',
-      version='0.0.4',
+      version='0.0.5',
       description='Humble Indie Bundle API client',
       author='Joel Pedraza',
       author_email='joel@joelpedraza.com',
       url='https://github.com/saik0/humblebundle-python',
-      download_url='https://github.com/saik0/humblebundle-python/tarball/0.0.4',
+      download_url='https://github.com/saik0/humblebundle-python/tarball/0.0.5',
       install_requires=['requests >= 2.0.0'],
       packages=['humblebundle'],
      )
